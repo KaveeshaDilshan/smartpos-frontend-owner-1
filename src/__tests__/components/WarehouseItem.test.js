b43b2a3edebc4a995f5f49ba92c4ee4dbdc6bca4
kaveeshadilshan <kaveesha.18@cse.mrt.ac.lk>
2021-10-30 13:25:58 +0530

    changes

--- src/__tests__/components/WarehouseItem.test.js
+++ src/__tests__/components/WarehouseItem.test.js
@@ -4,7 +4,8 @@ import { act } from 'react-dom/test-utils';
 import pretty from 'pretty';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
-import WarehouseItem from '../../views/managerPages/Warehouse/components/WarehouseItem';
+import { fireEvent } from '@testing-library/react';
+import ProductItem from '../../views/managerPages/Products/components/ProductItem';
 
 let container = null;
 const mockStore = configureStore([]);
@@ -14,11 +15,7 @@ beforeEach(() => {
   // setup a DOM element as a render target
   container = document.createElement('div');
   document.body.appendChild(container);
-  store = mockStore({
-    dashboardReducer: {
-      warehouseID: '123456789',
-    },
-  });
+  store = mockStore({});
 });
 
 afterEach(() => {
@@ -28,72 +25,68 @@ afterEach(() => {
   container = null;
 });
 
-const item = {
-  _id: '61555b6a7e8f8a491495386d',
-  product: {
+const product = {
+  archived: false,
+  _id: '6150ca6bf14212175cf8c577',
+  name: 'Maari Biscuit',
+  categoryId: {
     archived: false,
-    _id: '6150cabef14212175cf8c580',
-    name: 'Chocolate biscuit',
-    categoryId: '616c2ec3800c141704de18f6',
-    unitPrice: 100,
-    description: 'This is The chocolate biscuit',
-    photo: 'photo',
+    _id: '616c2ec3800c141704de18f6',
+    name: 'Biscuit',
     __v: 0,
   },
-  quantity: 275,
+  unitPrice: 120,
+  description: 'This is a Maari Biscuit',
+  photo: 'photo',
+  __v: 0,
 };
 
 describe('should render correctly', () => {
-  it('renders nothing when no warehouse product details has passed as props', () => {
+  it('renders nothing when no product details has passed as props', () => {
     act(() => {
       render(
         <Provider store={store}>
-          <WarehouseItem />
+          <ProductItem />
         </Provider>,
         container
       );
     });
     expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`""`);
   });
-  it('renders correctly when warehouse product details has passed as props', () => {
+  it('renders correctly when product details has passed as props', () => {
     act(() => {
       render(
         <Provider store={store}>
-          <WarehouseItem
-            productId={item.product._id}
-            poto={item.product.photo}
-            productName={item.product.name}
-            unitPrice={item.product.unitPrice}
-            count={item.quantity}
-          />
+          <ProductItem
+            productId={product._id}
+            name={product.name}
+            categoryName={product.categoryId.name}
+            unitPrice={product.unitPrice}
+            photoURL={product.photo}
+            description={product.description}
+          />{' '}
         </Provider>,
         container
       );
     });
     expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
-      "<div class=\\"MuiPaper-root MuiCard-root makeStyles-card-4 MuiPaper-elevation1 MuiPaper-rounded\\">
-        <div style=\\"display: flex;\\">
-          <div><img class=\\"MuiCardMedia-root makeStyles-media-6 MuiCardMedia-media MuiCardMedia-img\\" src=\\"photo\\" id=\\"photo\\" alt=\\"Product Image\\" title=\\"Product Image\\"></div>
-          <div style=\\"margin-left: 35px; display: flex; align-items: center; justify-content: space-between; width: 100%;\\">
-            <div style=\\"display: flex; align-items: center; justify-content: space-between; font-size: 20px; font-weight: 450; width: 50%;\\">
-              <div id=\\"product_name\\">Chocolate biscuit</div>
-              <div id=\\"unitPrice\\">Rs 100</div>
-            </div>
-            <div style=\\"margin-right: 10px; display: flex; align-items: center;\\">
-              <div class=\\"MuiFormControl-root MuiTextField-root\\">
-                <div class=\\"MuiInputBase-root MuiOutlinedInput-root MuiInputBase-formControl MuiInputBase-marginDense MuiOutlinedInput-marginDense\\"><input aria-invalid=\\"false\\" id=\\"quantity\\" type=\\"number\\" class=\\"MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputMarginDense MuiOutlinedInput-inputMarginDense\\" value=\\"275\\">
-                  <fieldset aria-hidden=\\"true\\" style=\\"padding-left: 8px;\\" class=\\"PrivateNotchedOutline-root-7 MuiOutlinedInput-notchedOutline\\">
-                    <legend class=\\"PrivateNotchedOutline-legend-8\\" style=\\"width: 0.01px;\\"><span>​</span></legend>
-                  </fieldset>
-                </div>
-              </div>
-              <div style=\\"display: flex; align-items: center;\\"><svg class=\\"MuiSvgIcon-root makeStyles-SaveIcon-5\\" focusable=\\"false\\" viewBox=\\"0 0 24 24\\" aria-hidden=\\"true\\">
-                  <path d=\\"M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z\\"></path>
-                </svg></div>
-            </div>
-          </div>
+      "<div class=\\"MuiPaper-root MuiCard-root makeStyles-root-9 MuiPaper-elevation1 MuiPaper-rounded\\"><img class=\\"MuiCardMedia-root makeStyles-media-12 MuiCardMedia-media MuiCardMedia-img\\" src=\\"photo\\" id=\\"photo\\" alt=\\"Product Image\\" title=\\"Product Image\\">
+        <hr class=\\"MuiDivider-root\\">
+        <hr class=\\"MuiDivider-root\\">
+        <div class=\\"MuiCardContent-root\\">
+          <p class=\\"MuiTypography-root makeStyles-name-10 MuiTypography-body1\\" id=\\"product_name\\">Maari Biscuit</p>
+          <p class=\\"MuiTypography-root makeStyles-categoryName-11 MuiTypography-body1 MuiTypography-colorTextSecondary MuiTypography-gutterBottom\\" id=\\"category_name\\">Biscuit</p>
         </div>
-      </div>"
+        <div class=\\"MuiCardContent-root\\">
+          <div class=\\"MuiChip-root makeStyles-priceTag-13 MuiChip-sizeSmall\\" id=\\"unitPrice\\"><span class=\\"MuiChip-label MuiChip-labelSmall\\">RS 120</span></div>
+        </div>
+        <div class=\\"MuiCardActions-root MuiCardActions-spacing\\"><button class=\\"MuiButtonBase-root MuiButton-root MuiButton-text makeStyles-button-14 MuiButton-textSizeSmall MuiButton-sizeSmall\\" tabindex=\\"0\\" type=\\"button\\" id=\\"detailsButton\\"><span class=\\"MuiButton-label\\">Details</span><span class=\\"MuiTouchRipple-root\\"></span></button><svg class=\\"MuiSvgIcon-root makeStyles-edit-15 MuiSvgIcon-colorAction\\" focusable=\\"false\\" viewBox=\\"0 0 24 24\\" aria-hidden=\\"true\\">
+            <path d=\\"M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z\\"></path>
+          </svg><svg class=\\"MuiSvgIcon-root makeStyles-delete-16 MuiSvgIcon-colorAction\\" focusable=\\"false\\" viewBox=\\"0 0 24 24\\" aria-hidden=\\"true\\">
+            <path d=\\"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z\\"></path>
+          </svg></div>
+      </div>
+      <div></div>"
     `);
   });
 });
@@ -103,56 +96,84 @@ describe('should render the correct values which have been passed', () => {
     act(() => {
       render(
         <Provider store={store}>
-          <WarehouseItem
-            productId={item.product._id}
-            poto={item.product.photo}
-            productName={item.product.name}
-            unitPrice={item.product.unitPrice}
-            count={item.quantity}
+          <ProductItem
+            productId={product._id}
+            name={product.name}
+            categoryName={product.categoryId.name}
+            unitPrice={product.unitPrice}
+            photoURL={product.photo}
+            description={product.description}
           />
         </Provider>,
         container
       );
     });
     expect(container.querySelector('#product_name').textContent).toBe(
-      `${item.product.name}`
+      `${product.name}`
+    );
+  });
+  it('should have correct category name', () => {
+    act(() => {
+      render(
+        <Provider store={store}>
+          <ProductItem
+            productId={product._id}
+            name={product.name}
+            categoryName={product.categoryId.name}
+            unitPrice={product.unitPrice}
+            photoURL={product.photo}
+            description={product.description}
+          />
+        </Provider>,
+        container
+      );
+    });
+    expect(container.querySelector('#category_name').textContent).toBe(
+      `${product.categoryId.name}`
     );
   });
   it('should have correct unit price', () => {
     act(() => {
       render(
         <Provider store={store}>
-          <WarehouseItem
-            productId={item.product._id}
-            poto={item.product.photo}
-            productName={item.product.name}
-            unitPrice={item.product.unitPrice}
-            count={item.quantity}
+          <ProductItem
+            productId={product._id}
+            name={product.name}
+            categoryName={product.categoryId.name}
+            unitPrice={product.unitPrice}
+            photoURL={product.photo}
+            description={product.description}
           />
         </Provider>,
         container
       );
     });
     expect(container.querySelector('#unitPrice').textContent).toBe(
-      `Rs ${item.product.unitPrice}`
+      `RS ${product.unitPrice}`
     );
   });
-
-  it('should have correct quantity', () => {
-    act(() => {
-      render(
-        <Provider store={store}>
-          <WarehouseItem
-            productId={item.product._id}
-            poto={item.product.photo}
-            productName={item.product.name}
-            unitPrice={item.product.unitPrice}
-            count={item.quantity}
-          />
-        </Provider>,
-        container
-      );
-    });
-    expect(container.querySelector('#quantity').textContent).toBe(``);
-  });
 });
+
+// describe('should call the function', () => {
+//   it('should call the function on click on the product Item', () => {
+//     let button;
+//     act(() => {
+//       render(
+//         <Provider store={store}>
+//           <ProductItem
+//             productId={product._id}
+//             name={product.name}
+//             categoryName={product.categoryId.name}
+//             unitPrice={product.unitPrice}
+//             photoURL={product.photo}
+//             description={product.description}
+//           />
+//         </Provider>,
+//         container
+//       );
+//       button = container.querySelector('#detailsButton');
+//       fireEvent.click(button);
+//     });
+//     expect(button).toHaveBeenCalledTimes(1);
+//   });
+// });
